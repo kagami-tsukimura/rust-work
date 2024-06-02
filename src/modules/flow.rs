7ba455152ba93dcb533fcc999531b534cacd4241
Kagami <shimura.kyohei7617@gmail.com>
2024-06-02 18:39:21 +0900

    auto: loop

--- src/modules/flow.rs
+++ src/modules/flow.rs
@@ -61,10 +61,10 @@ fn loops() {
     println!("-----");
     let mut x = 0;
     loop {
+        x += 1;
         if x > 10 {
             break;
         }
-        println!("{}", x);
-        x += 1;
+        println!("Hello: {}", x);
     }
 }
