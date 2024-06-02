0810cadf483c1233e7e31fca5e5a8dfc018029f5
Kagami <shimura.kyohei7617@gmail.com>
2024-06-02 12:21:45 +0900

    auto: commentout

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,11 @@
 fn main() {
     println!("Hello, world!");
     println!("Hello, {}!", "world");
+    /*
+    println!("Hello1, {}!", "world");
+    println!("Hello2, {}!", "world");
+    println!("Hello3, {}!", "world");
+     */
     // 改行なし
     print!("Hello, ");
     print!("Rust");
