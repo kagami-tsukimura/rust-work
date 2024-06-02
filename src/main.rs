3c0434aaecb5f7fde563faf1c0034b307f3f86ce
Kagami <shimura.kyohei7617@gmail.com>
2024-06-02 17:50:01 +0900

    auto: flow

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
 mod modules {
     pub mod basic;
+    pub mod flow;
 }
 
 fn main() {
     modules::basic::main();
+    modules::flow::main();
 }
---
+++ src/modules/flow.rs
@@ -0,0 +1,8 @@
+pub fn main() {
+    Sentence();
+}
+
+fn Sentence() {
+    println!("-----");
+    println!("Hello, sentences!");
+}
