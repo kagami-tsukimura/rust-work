bbb028b8af0f1ac01e32beb4d6d9be22d16e0126
Kagami <shimura.kyohei7617@gmail.com>
2024-06-02 14:05:57 +0900

    auto: module分割

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,22 @@
 fn main() {
     print_msg();
+    print_variable();
+}
 
+fn print_msg() {
+    println!("Hello, world!");
+    println!("Hello, {}!", "world");
+    /*
+    println!("Hello1, {}!", "world");
+    println!("Hello2, {}!", "world");
+    println!("Hello3, {}!", "world");
+     */
+    // 改行なし
+    print!("Hello, ");
+    print!("Rust");
+}
+
+fn print_variable() {
     // 書き換えng
     let a: i32 = 1;
     println!("{}", a);
@@ -19,17 +35,4 @@ fn main() {
     // 変数のシャドーイング（型書き換え）: rustではok
     let c: &str = "hello";
     println!("{}", c);
-}
-
-fn print_msg() {
-    println!("Hello, world!");
-    println!("Hello, {}!", "world");
-    /*
-    println!("Hello1, {}!", "world");
-    println!("Hello2, {}!", "world");
-    println!("Hello3, {}!", "world");
-     */
-    // 改行なし
-    print!("Hello, ");
-    print!("Rust");
 }
