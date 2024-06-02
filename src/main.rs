c7c08701281ca0d8acee314aef4068be08fb3220
Kagami <shimura.kyohei7617@gmail.com>
2024-06-02 14:05:27 +0900

    auto: print

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,24 @@
 fn main() {
     print_msg();
+
+    // 書き換えng
+    let a: i32 = 1;
+    println!("{}", a);
+    // a = 2;
+
+    // mut: 書き換えok
+    let mut b: i32 = 1;
+    println!("before: {}", b);
+    b = 2;
+    println!("after: {}", b);
+
+    let c: i32;
+    c = 1;
+    println!("{}", c);
+
+    // 変数のシャドーイング（型書き換え）: rustではok
+    let c: &str = "hello";
+    println!("{}", c);
 }
 
 fn print_msg() {
