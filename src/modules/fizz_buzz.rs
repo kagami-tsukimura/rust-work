37fc5bb562c8b7dae09955c6fdd131be377f727d
Kagami <shimura.kyohei7617@gmail.com>
2024-06-02 21:07:41 +0900

    auto: fizzbuzz

--- src/modules/fizz_buzz.rs
+++ src/modules/fizz_buzz.rs
@@ -1,5 +1,17 @@
 pub fn main() {
-    fizzbuzz();
+    fizzbuzz(20);
 }
 
-fn fizzbuzz() {}
+fn fizzbuzz(count: i32) {
+    for i in 1..=count {
+        if i % 3 == 0 && i % 5 == 0 {
+            println!("FizzBuzz")
+        } else if i % 3 == 0 {
+            println!("Fizz")
+        } else if i % 5 == 0 {
+            println!("Buzz")
+        } else {
+            println!("{}", i)
+        }
+    }
+}
