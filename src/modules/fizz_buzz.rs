8840c9313450da586644f3d7c84636922c53441a
Kagami <shimura.kyohei7617@gmail.com>
2024-06-02 21:10:13 +0900

    auto: fizzbuzz

--- src/modules/fizz_buzz.rs
+++ src/modules/fizz_buzz.rs
@@ -1,8 +1,10 @@
 pub fn main(count: i32) {
-    fizzbuzz(count);
+    fizzbuzz_for(count);
+    println!("-----");
+    fizbuzz_while(count);
 }
 
-fn fizzbuzz(count: i32) {
+fn fizzbuzz_for(count: i32) {
     for i in 1..=count {
         if i % 3 == 0 && i % 5 == 0 {
             println!("FizzBuzz")
@@ -13,5 +15,21 @@ fn fizzbuzz(count: i32) {
         } else {
             println!("{}", i)
         }
+    }
+}
+
+fn fizbuzz_while(count: i32) {
+    let mut i = 1;
+    while count > i {
+        if i % 3 == 0 && i % 5 == 0 {
+            println!("FizzBuzz")
+        } else if i % 3 == 0 {
+            println!("Fizz")
+        } else if i % 5 == 0 {
+            println!("Buzz")
+        } else {
+            println!("{}", i)
+        }
+        i += 1;
     }
 }
