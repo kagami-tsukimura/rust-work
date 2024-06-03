e79cbb80ff34f4ce30d7cb27a342404b44ccf2ae
Kagami <shimura.kyohei7617@gmail.com>
2024-06-03 21:36:57 +0900

    auto: reference

--- src/modules/refs.rs
+++ src/modules/refs.rs
@@ -19,4 +19,18 @@ fn references() {
     *b += 1;
     // println!("{:?}", a);
     println!("{:?}", b);
+
+    println!("-----");
+
+    let s1 = String::from("Hello");
+    let s2 = String::from("Rust");
+    let s = concat(&s1, &s2);
+    println!("{}", s);
+    println!("{}", s1);
+    println!("{}", s2);
+}
+
+fn concat(a: &String, b: &String) -> String {
+    let c: String = format!("{}, {}", a, b);
+    c
 }
