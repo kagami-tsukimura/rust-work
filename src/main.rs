7b5c5a00b8575aa503aa47648bdbf2c65ae6492b
Kagami <shimura.kyohei7617@gmail.com>
2024-06-03 22:36:46 +0900

    auto: struct

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,8 @@ mod modules {
     // pub mod fizz_buzz;
     // pub mod ownership;
     // pub mod refs;
-    pub mod smart_pointer;
+    // pub mod smart_pointer;
+    pub mod structs;
 }
 
 fn main() {
@@ -13,5 +14,6 @@ fn main() {
     // modules::fizz_buzz::main(20);
     // modules::ownership::main();
     // modules::refs::main();
-    modules::smart_pointer::main();
+    // modules::smart_pointer::main();
+    modules::structs::main();
 }
---
+++ src/modules/structs.rs
@@ -0,0 +1,12 @@
+struct Rectangle {
+    width: u32,
+    height: u32,
+}
+
+pub fn main() {
+    structs();
+}
+
+fn structs() {
+    println!("-----");
+}
