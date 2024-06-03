2416070d2468b303314631281d687d2b485a6151
Kagami <shimura.kyohei7617@gmail.com>
2024-06-03 21:34:56 +0900

    auto: reference

--- src/modules/refs.rs
+++ src/modules/refs.rs
@@ -4,10 +4,19 @@ pub fn main() {
 
 fn references() {
     let x = 1;
-    // 共有参照
+    // 共有参照: 読み出し可、変更不可
     let y = &x;
     let z = &x;
     println!("{:?}", x);
     println!("{:?}", y);
     println!("{:?}", z);
+
+    println!("-----");
+
+    // 可変参照: 読み出し可、変更可
+    let mut a = 1;
+    let b = &mut a;
+    *b += 1;
+    println!("{:?}", a);
+    println!("{:?}", b);
 }
