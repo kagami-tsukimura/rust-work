c9c28af119e1bfc479614d4bd596f807664edb84
Kagami <shimura.kyohei7617@gmail.com>
2024-06-03 21:12:50 +0900

    auto: ownership

--- src/modules/ownership.rs
+++ src/modules/ownership.rs
@@ -5,12 +5,13 @@ pub fn main() {
 fn stacks() {
     let mut v1 = vec![1, 2, 3];
     println!("v1 ptr: {:?}", v1.as_ptr());
-    println!("v1[0]: {:p}", &v1[0]);
+    println!("v1[0] : {:p}", &v1[0]);
 
     println!("v1 len: {}", v1.len());
     println!("v1 cap: {}", v1.capacity());
 
     v1.push(4);
+    println!("v1 ptr: {:?}", v1.as_ptr());
     println!("v1 len: {}", v1.len());
     println!("v1 cap: {}", v1.capacity());
 }
