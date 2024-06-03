8936757b6492bd03536663aa20b5ec00b6f38c6a
Kagami <shimura.kyohei7617@gmail.com>
2024-06-03 21:46:16 +0900

    auto: smartpointer

--- src/modules/smart_pointer.rs
+++ src/modules/smart_pointer.rs
@@ -8,4 +8,9 @@ fn smart_pointer() {
     let x = Box::new(10);
     println!("x_box: {:p}", x);
     println!("*x + 2 = {}", *x + 2);
+
+    // rc: 値の参照数をカウント(references count)
+    let y = std::rc::Rc::new(10);
+    println!("y_rc: {:p}", y);
+    println!("*y + 2 = {}", *y + 2);
 }
