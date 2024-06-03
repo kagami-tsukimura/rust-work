d5420d806059fe36ce8013230030e2ebc1fc7566
Kagami <shimura.kyohei7617@gmail.com>
2024-06-03 21:45:53 +0900

    auto: smartpointer

--- src/modules/smart_pointer.rs
+++ src/modules/smart_pointer.rs
@@ -4,7 +4,7 @@ pub fn main() {
 
 fn smart_pointer() {
     // スマートポインタ: データの所有をするポインタ
-    // box: コンパイル時に動的に確保
+    // box: サイズが未確定でもコンパイル→ヒープに確保
     let x = Box::new(10);
     println!("x_box: {:p}", x);
     println!("*x + 2 = {}", *x + 2);
