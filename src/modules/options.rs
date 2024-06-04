c1645631817cda09af8b2c95f358f59225e3cd00
Kagami <shimura.kyohei7617@gmail.com>
2024-06-04 19:22:25 +0900

    auto: option

--- src/modules/options.rs
+++ src/modules/options.rs
@@ -1,3 +1,10 @@
 pub fn main() {
-    println!("-----");
+    options();
+}
+
+fn options() {
+    let x = Some(5);
+    let y: Option<i32> = None;
+    println!("{:?}", x);
+    println!("{:?}", y);
 }
