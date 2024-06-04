a5e5cc3e23468cd17d93c71e03a2573c1f7cbd17
Kagami <shimura.kyohei7617@gmail.com>
2024-06-04 21:14:28 +0900

    auto: modules

--- src/modules/mods.rs
+++ src/modules/mods.rs
@@ -26,5 +26,12 @@ pub fn main() {
 
 fn mods() {
     // crate: main.rsからの絶対パスを指定
+    // NOTE: main.rsから、modules/mods(.rs)の、test_moduleの、sub_module1の、test_fn1を呼び出す
     crate::modules::mods::test_module::sub_module1::test_fn1();
+    // // 関数がprivateのためアクセス不可
+    // crate::modules::mods::test_module::sub_module1::test_fn2();
+    // // モジュールがprivateのためアクセス不可
+    // crate::modules::mods::test_module::sub_module2::test_fn1();
+    // // モジュール、関数がprivateのためアクセス不可
+    // crate::modules::mods::test_module::sub_module2::test_fn2();
 }
