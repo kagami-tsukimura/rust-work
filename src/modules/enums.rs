b94ac58aad6970e015a3f3d5f4240734f197761c
Kagami <shimura.kyohei7617@gmail.com>
2024-06-04 19:18:56 +0900

    auto: enum

--- src/modules/enums.rs
+++ src/modules/enums.rs
@@ -1,9 +1,21 @@
 enum Shape {
     Circle,
-    Square,
-    Triangle,
+    Square(u32),
+    Triangle { base: u32, heght: u32 },
 }
 
 pub fn main() {
-    println!("-----");
+    let c = Shape::Circle;
+    let s: Shape = Shape::Square(1);
+    let t = Shape::Triangle { base: 10, heght: 5 };
+
+    c.sample_method();
+    s.sample_method();
+    t.sample_method();
+}
+
+impl Shape {
+    fn sample_method(&self) {
+        println!("call sample_method");
+    }
 }
