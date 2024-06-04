914133fc51b4d22af2f9e0a36bfce5ab9ebdb8a8
Kagami <shimura.kyohei7617@gmail.com>
2024-06-04 19:10:24 +0900

    auto: struct method

--- src/modules/structs.rs
+++ src/modules/structs.rs
@@ -8,6 +8,11 @@ impl Rectangle {
     fn area(&self) -> u32 {
         self.width * self.height
     }
+
+    // 型関連関数: selfなし
+    fn new(width: u32, height: u32) -> Rectangle {
+        Rectangle { width, height }
+    }
 }
 
 pub fn main() {
@@ -31,4 +36,11 @@ fn structs() {
     println!("area: {}", rectangle.area());
 
     println!("-----");
+    type_relation();
+}
+
+fn type_relation() {
+    let rectangle = Rectangle::new(10, 50);
+
+    println!("rectanfle_area: {}", rectangle.area());
 }
