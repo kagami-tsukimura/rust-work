f8418c223493cd499bc9fb17ad70bb5bbc6f4f09
Kagami <shimura.kyohei7617@gmail.com>
2024-06-04 21:11:46 +0900

    auto: modules

--- src/modules/mods.rs
+++ src/modules/mods.rs
@@ -1,7 +1,30 @@
+mod test_module {
+    pub mod sub_module1 {
+        pub fn test_fn1() {
+            println!("test_fn1-1");
+        }
+
+        fn test_fn2() {
+            println!("test_fn1-2");
+        }
+    }
+
+    mod sub_module2 {
+        pub fn test_fn1() {
+            println!("test_fn1-2");
+        }
+
+        fn test_fn2() {
+            println!("test_fn2-2");
+        }
+    }
+}
+
 pub fn main() {
     mods();
 }
 
 fn mods() {
-    println!("-----");
+    // crate: main.rsからの絶対パスを指定
+    crate::modules::test_module::sub_module1::test_fn1();
 }
