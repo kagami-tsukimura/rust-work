9a4c8d8de1603ff106174da4d4d3f6abd2f41870
Kagami <shimura.kyohei7617@gmail.com>
2024-06-05 22:30:33 +0900

    auto: results

--- src/modules/results.rs
+++ src/modules/results.rs
@@ -2,10 +2,4 @@ pub fn main() {
     results();
 }
 
-fn results() {
-    let result = divide(10, 2);
-    match result {
-        Ok(x) => println!("Result: {}", x),
-        Err(e) => println!("Error: {}", e),
-    }
-}
+fn results() {}
