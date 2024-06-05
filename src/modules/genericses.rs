e1516346ff5249b4e41a10cba5744035b7dcb9ba
Kagami <shimura.kyohei7617@gmail.com>
2024-06-05 22:00:38 +0900

    auto: generics

--- src/modules/genericses.rs
+++ src/modules/genericses.rs
@@ -5,6 +5,7 @@ pub fn main() {
 fn genericses() {
     println!("{}", max(1, 2));
     println!("{}", max(1.0, 2.3));
+    println!("{}", max("hoge", "huga"));
 }
 
 // generics境界: T: PartialOrdで引数が比較可能な型に制限する
