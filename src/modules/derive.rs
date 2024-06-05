718895cd95f6dc451901d5f4c3dcae9cf6a036df
Kagami <shimura.kyohei7617@gmail.com>
2024-06-05 21:51:16 +0900

    auto: derive

--- src/modules/derive.rs
+++ src/modules/derive.rs
@@ -7,10 +7,15 @@ fn derive() {
     println!("{:?}", (1, 2, 3));
 
     // debug trait
-    #[derive(Debug)]
+    #[derive(Debug, PartialEq)]
     struct S {
         val1: i32,
         val2: i32,
     }
     println!("{:?}", S { val1: 1, val2: 2 });
+
+    let s1 = S { val1: 1, val2: 2 };
+    let s2 = S { val1: 1, val2: 3 };
+
+    println!("{}", s1 == s2);
 }
