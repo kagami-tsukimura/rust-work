f65975846352037f43ecca04b25e9308517c2047
Kagami <shimura.kyohei7617@gmail.com>
2024-06-05 22:00:05 +0900

    auto: generics

--- src/modules/genericses.rs
+++ src/modules/genericses.rs
@@ -7,7 +7,12 @@ fn genericses() {
     println!("{}", max(1.0, 2.3));
 }
 
-fn max<T: PartialOrd>(a: T, b: T) -> T {
+// generics境界: T: PartialOrdで引数が比較可能な型に制限する
+fn max<T: PartialOrd>(a: T, b: T) -> T
+// where T: PartialOrd
+// NOTE: where T: PartialOrdは、<T: PartialOrd>と同様。
+// NOTE: 単純な制約なら<>で、複数の制約をつけたい等で複雑な制約ならwhereを使う。
+{
     if a >= b {
         a
     } else {
