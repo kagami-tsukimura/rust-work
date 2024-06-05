e1cbd25940de12ac53739c87fde087e60a9136a7
Kagami <shimura.kyohei7617@gmail.com>
2024-06-05 21:10:06 +0900

    auto: args

--- src/modules/lib_args.rs
+++ src/modules/lib_args.rs
@@ -1,7 +1,16 @@
+use rust_work::sample_trait::{double_area, Circle, Rectangle, Shape};
 pub fn main() {
     lib_args();
 }
 
 fn lib_args() {
-    println!("Hello, lib_args!");
+    let rect = Rectangle {
+        width: 4.0,
+        height: 5.0,
+    };
+
+    let circle = Circle { radius: 3.0 };
+
+    println!("Rectangle double_area: {}", double_area(&rect));
+    println!("Circle double_area: {}", double_area(&circle));
 }
