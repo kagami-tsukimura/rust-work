a68eb80f729a4062ddea5872328c49d405c1e764
Kagami <shimura.kyohei7617@gmail.com>
2024-06-05 21:57:38 +0900

    auto: generics

--- src/modules/genericses.rs
+++ src/modules/genericses.rs
@@ -4,9 +4,10 @@ pub fn main() {
 
 fn genericses() {
     println!("{}", max(1, 2));
+    println!("{}", max(1.0, 2.3));
 }
 
-fn max<T>(a: T, b: T) -> T {
+fn max<T: PartialOrd>(a: T, b: T) -> T {
     if a >= b {
         a
     } else {
