585e9bef3c63bc918a58733f7ce258117aa3d2f9
Kagami <shimura.kyohei7617@gmail.com>
2024-06-08 08:17:28 +0900

    auto: vector

--- src/modules/vectors2.rs
+++ src/modules/vectors2.rs
@@ -5,6 +5,7 @@ pub fn main() {
 fn vectors2() {
     let v1 = vec!["Rust", "Python", "Java"];
     let v2 = vec!["C#", "Go"];
+    // vector結合
     let v3 = [v1, v2].concat();
 
     println!("{:?}", v3);
