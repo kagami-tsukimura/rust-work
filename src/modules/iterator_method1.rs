aede48bb28d493af7909466b9add79e78b834c45
Kagami <shimura.kyohei7617@gmail.com>
2024-06-08 01:45:46 +0900

    auto: counter iterate

--- src/modules/iterator_method1.rs
+++ src/modules/iterator_method1.rs
@@ -5,6 +5,7 @@ pub fn main() {
 fn iterator_method1() {
     let v = vec![1, 2, 3, 4, 5];
     let m = v.iter().map(|x| x * 2);
+    // イテレータの消費
     for val in m {
         println!("{}", val);
     }
