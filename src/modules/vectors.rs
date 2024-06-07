363bf90893d81ab565859fd49cf055e50062ad0f
Kagami <shimura.kyohei7617@gmail.com>
2024-06-08 08:07:33 +0900

    auto: vector

--- src/modules/vectors.rs
+++ src/modules/vectors.rs
@@ -8,4 +8,7 @@ fn vectors() {
     println!("{:?}", v1.as_ptr());
     println!("{:?}", v1.len());
     println!("{:?}", v1.capacity());
+
+    let v2 = vec!["Rust", "Python", "Java"];
+    println!("{:?}", &v2[0]);
 }
