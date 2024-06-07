b5c49ef522958edf107a8da301261edd58b36e0f
Kagami <shimura.kyohei7617@gmail.com>
2024-06-07 23:20:10 +0900

    auto: counter iterate

--- src/modules/counter_iterate.rs
+++ src/modules/counter_iterate.rs
@@ -1,3 +1,22 @@
+struct Counter {
+    start: u32,
+    end: u32,
+}
+
+impl Iterator for Counter {
+    type Item = u32;
+
+    fn next(&mut self) -> Option<u32> {
+        if self.start >= self.end {
+            None
+        } else {
+            let result = Some(self.start);
+            self.start += 1;
+            result
+        }
+    }
+}
+
 pub fn main() {
     counter_iterate();
 }
