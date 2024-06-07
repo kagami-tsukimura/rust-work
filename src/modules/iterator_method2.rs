0d4e482c2052f9ce644a6f5e39cb1251987ea489
Kagami <shimura.kyohei7617@gmail.com>
2024-06-08 01:54:37 +0900

    auto: iterator method

--- src/modules/iterator_method2.rs
+++ src/modules/iterator_method2.rs
@@ -4,17 +4,8 @@ pub fn main() {
 
 fn iterator_method2() {
     let v = vec![1, 2, 3, 4, 5];
-    // map
-    let m = v.iter().map(|x| x * 2);
-    // イテレータの消費
-    for val in m {
-        println!("{}", val);
-    }
-
-    let c: Vec<_> = v.iter().map(|x| x * 2).collect();
+    // イテレータを消費するメソッド: count
+    // count
+    let c = v.iter().count();
     println!("{:?}", c);
-
-    // filter
-    let f: Vec<_> = v.iter().filter(|x| *x % 2 != 0).collect();
-    println!("{:?}", f);
 }
