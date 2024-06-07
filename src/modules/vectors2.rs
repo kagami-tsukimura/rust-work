ef1e86d19ce5d07c5d852ba1a78ce9e472856167
Kagami <shimura.kyohei7617@gmail.com>
2024-06-08 08:20:08 +0900

    auto: vector

--- src/modules/vectors2.rs
+++ src/modules/vectors2.rs
@@ -22,7 +22,7 @@ fn vectors2() {
     println!("-----");
 
     let mut v6 = vec![3, 6, 1, 7, 2];
+    println!("beore sort: {:?}", v6);
     v6.sort();
-
-    println!("{:?}", v6);
+    println!("after sort: {:?}", v6);
 }
