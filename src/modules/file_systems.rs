e16ad5279f0ae082e10287f5780595a3d18ed769
Kagami <shimura.kyohei7617@gmail.com>
2024-06-08 21:22:47 +0900

    auto: filesystems

--- src/modules/file_systems.rs
+++ src/modules/file_systems.rs
@@ -6,5 +6,5 @@ pub fn main() {
 
 fn file_systems() {
     fs::create_dir("src/test1").unwrap();
-    fs::remove_dir_all("src/test2/test2-1/test2-2").unwrap();
+    fs::create_dir_all("src/test2/test2-1/test2-2").unwrap();
 }
