22430a1a78f518efbd285a0114c6cd4cf55f9d06
Kagami <shimura.kyohei7617@gmail.com>
2024-06-08 19:20:55 +0900

    auto: output files

--- src/modules/outfiles.rs
+++ src/modules/outfiles.rs
@@ -1,7 +1,12 @@
+use std::fs::File;
+use std::io::prelude::*;
+
 pub fn main() {
     outfiles();
 }
 
 fn outfiles() {
+    let path1 = "output_sample1.txt";
+    let mut f1 = File::create(path1).unwrap();
     println!("-----");
 }
