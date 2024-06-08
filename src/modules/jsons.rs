d63d3f4e479e5718a4e9da5582971c17538dcda6
Kagami <shimura.kyohei7617@gmail.com>
2024-06-08 20:42:46 +0900

    auto: json

--- src/modules/jsons.rs
+++ src/modules/jsons.rs
@@ -1,4 +1,6 @@
 use serde::{Deserialize, Serialize};
+use std::fs::File;
+use std::io::prelude::*;
 
 #[derive(Serialize, Deserialize, Debug)]
 struct Person {
@@ -17,6 +19,10 @@ fn jsons() {
         age: 28,
         phones: vec![String::from("080-1234-5678"), String::from("090-1234-5678")],
     };
-    let json_date = serde_json::to_string_pretty(&p).unwrap();
-    println!("{}", json_date);
+    let json_data = serde_json::to_string_pretty(&p).unwrap();
+    println!("{}", json_data);
+
+    let file_path = "sample.json";
+    let mut f = File::create(file_path).unwrap();
+    writeln!(f, "{}", json_data).unwrap();
 }
