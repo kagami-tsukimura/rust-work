9c4a9534079e957a62986f74f4ace154af47e49e
Kagami <shimura.kyohei7617@gmail.com>
2024-06-08 11:33:30 +0900

    auto: document test

--- src/lib.rs
+++ src/lib.rs
@@ -72,7 +72,7 @@ fn say_bye() {
 ///
 /// ```
 /// let result = rust_work::add(1, 2);
-/// assert_eq!(result, 3);
+/// assert_eq!(result, 4);
 /// ```
 pub fn add(a: i32, b: i32) -> i32 {
     a + b
