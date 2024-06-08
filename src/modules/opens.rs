646ab1151283a58c4cd17863dcf9454e67853dae
Kagami <shimura.kyohei7617@gmail.com>
2024-06-08 19:35:23 +0900

    auto: open files

--- src/modules/opens.rs
+++ src/modules/opens.rs
@@ -5,11 +5,19 @@ pub fn main() {
 }
 
 fn opens() {
-    let file_path = "sample1.txt";
-
-    let f = OpenOptions::new()
+    let file_path1 = "sample1.txt";
+    let f1 = OpenOptions::new()
         // 追記
         .append(true)
-        .open(file_path)
+        .open(file_path1)
+        .unwrap();
+
+    let file_path2 = "sample2.txt";
+    let f2 = OpenOptions::new()
+        // 上書き
+        .write(true)
+        // ファイルが存在すればエラー
+        .create_new(true)
+        .open(file_path2)
         .unwrap();
 }
