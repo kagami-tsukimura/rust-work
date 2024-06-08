c37a1190fae826b5be6be79dfa58972713eb0dbd
Kagami <shimura.kyohei7617@gmail.com>
2024-06-08 11:15:52 +0900

    auto: test

--- src/tests/unit_tests.rs
+++ src/tests/unit_tests.rs
@@ -1,13 +1,22 @@
 #[test]
 // cargo test main.rs
-fn test_sample() {
+fn test_eq() {
     let a = 1 + 1;
     let b = 2;
-    let c = 2 + 2;
 
     assert_eq!(a, b);
-    // assert_eq!(a, c);
+}
+#[test]
+fn test_ne() {
+    let a = 1 + 1;
+    let c = 2 + 2;
+
     assert_ne!(a, c);
+}
+#[test]
+fn test_vanilla() {
+    let c = 2 + 2;
+
     assert!(c == 4);
 }
 
