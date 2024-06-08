3d1c1f04a02e94778497bf88fda48ca6a39071d1
Kagami <shimura.kyohei7617@gmail.com>
2024-06-08 09:02:30 +0900

    auto: queue

--- src/modules/queues.rs
+++ src/modules/queues.rs
@@ -1,7 +1,16 @@
+use std::collections::VecDeque;
+
 pub fn main() {
     queues();
 }
 
 fn queues() {
-    println!("queues");
+    let mut q = VecDeque::new();
+    q.push_back(1);
+    q.push_back(2);
+    q.push_back(3);
+    println!("{:?}", q);
+    let x = q.pop_front();
+    println!("{:?}", x);
+    println!("{:?}", q);
 }
