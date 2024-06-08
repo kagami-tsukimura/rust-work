1339dbf9752052e67e22d1ff99eee8bf9ea10ec9
Kagami <shimura.kyohei7617@gmail.com>
2024-06-08 10:41:01 +0900

    auto: set

--- src/modules/sets.rs
+++ src/modules/sets.rs
@@ -5,13 +5,30 @@ pub fn main() {
 }
 
 fn sets() {
-    let mut s = HashSet::new();
+    let mut s1 = HashSet::new();
     // 重複はしない
-    s.insert(1);
-    s.insert(1);
-    s.insert(1);
-    s.insert(2);
-    s.insert(3);
+    s1.insert(1);
+    s1.insert(1);
+    s1.insert(1);
+    s1.insert(2);
+    s1.insert(3);
     // HashSet: 順序はランダム
-    println!("{:?}", s);
+    println!("{:?}", s1);
+
+    println!("{:?}", s1.contains(&2));
+    println!("{:?}", s1.remove(&2));
+    println!("{:?}", s1);
+
+    println!("-----");
+
+    let mut s2 = HashSet::new();
+    s2.insert(1);
+    s2.insert(2);
+    s2.insert(3);
+    s2.insert(5);
+
+    let s3 = &s1 | &s2;
+    println!("set1: {:?}", s1);
+    println!("set2: {:?}", s2);
+    println!("set3(set1 + set2): {:?}", s3);
 }
