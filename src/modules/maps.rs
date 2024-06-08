fff48640b48700769df93dbee5a44d38fa9490d1
Kagami <shimura.kyohei7617@gmail.com>
2024-06-08 10:32:59 +0900

    auto: map

--- src/modules/maps.rs
+++ src/modules/maps.rs
@@ -1,11 +1,14 @@
+use std::collections::HashMap;
+
 pub fn main() {
     maps();
 }
 
 fn maps() {
-    let mut m = std::collections::HashMap::new();
-    m.insert("Rust", "Programming");
-    m.insert("Python", "Programming");
-    m.insert("Java", "Programming");
+    let mut m = HashMap::new();
+    m.insert("Japan", 12);
+    m.insert("USA", 3);
+    m.insert("China", 2);
+    m.insert("India", 1);
     println!("{:?}", m);
 }
