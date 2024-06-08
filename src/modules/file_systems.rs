3fbe5cacea5ec52c42d2f86e0c75784fda549fe1
Kagami <shimura.kyohei7617@gmail.com>
2024-06-08 21:31:08 +0900

    auto: filesystems

--- src/modules/file_systems.rs
+++ src/modules/file_systems.rs
@@ -1,4 +1,5 @@
 use std::fs;
+use std::os::unix::fs::PermissionsExt;
 
 pub fn main() {
     file_systems();
@@ -14,14 +15,19 @@ fn file_systems() {
     let file3 = "src/sample3.txt";
     let mvfile = "src/test1/sample3.txt";
 
-    fs::create_dir(dir1).unwrap();
-    // fs::create_dir_all(dir2).unwrap();
+    let mut permissons = fs::metadata(mvfile).unwrap().permissions();
+    permissons.set_mode(0o600);
 
-    // fs::remove_dir(dir1).unwrap();
-    // fs::remove_dir_all(dir3).unwrap();
+    // fs::create_dir(dir1).unwrap();
+    // // fs::create_dir_all(dir2).unwrap();
 
-    // // fs::remove_file(file1).unwrap();
-    // fs::copy(file2, file3).unwrap();
+    // // fs::remove_dir(dir1).unwrap();
+    // // fs::remove_dir_all(dir3).unwrap();
 
-    fs::rename(file3, mvfile).unwrap();
+    // // // fs::remove_file(file1).unwrap();
+    // // fs::copy(file2, file3).unwrap();
+
+    // fs::rename(file3, mvfile).unwrap();
+
+    fs::set_permissions(mvfile, permissons).unwrap();
 }
