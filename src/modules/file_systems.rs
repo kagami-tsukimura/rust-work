085383accd244334af4b10af59e78e956b14d663
Kagami <shimura.kyohei7617@gmail.com>
2024-06-08 21:27:00 +0900

    auto: filesystems

--- src/modules/file_systems.rs
+++ src/modules/file_systems.rs
@@ -9,9 +9,16 @@ fn file_systems() {
     let dir2 = "src/test2/test2-1/test2-2";
     let dir3 = "src/test2/";
 
+    let file1 = "src/sample1.txt";
+    let file2 = "src/sample2.txt";
+    let file3 = "src/sample3.txt";
+
     fs::create_dir(dir1).unwrap();
     fs::create_dir_all(dir2).unwrap();
 
     fs::remove_dir(dir1).unwrap();
     fs::remove_dir_all(dir3).unwrap();
+
+    // fs::remove_file(file1).unwrap();
+    fs::copy(file2, file3).unwrap();
 }
