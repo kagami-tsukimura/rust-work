be0c50235faa4be2a8baf13ff8ad4a0336c5d0ce
Kagami <shimura.kyohei7617@gmail.com>
2024-06-08 11:24:06 +0900

    auto: test

--- src/tests/panic_test.rs
+++ src/tests/panic_test.rs
@@ -10,8 +10,8 @@ fn maybe_panic(flag: bool) {
 #[test]
 #[should_panic(expected = "flag is true")]
 fn test_maybe_panic() {
-    maybe_panic(false);
-    // maybe_panic(true);
+    // maybe_panic(false);
+    maybe_panic(true);
 }
 
 pub fn main() {}
