92cd05a181b568c9c63abdbfea7abecabea39e3b
Kagami <shimura.kyohei7617@gmail.com>
2024-06-08 09:03:14 +0900

    auto: queue

--- src/modules/queues.rs
+++ src/modules/queues.rs
@@ -7,4 +7,9 @@ pub fn main() {
 fn queues() {
     let mut q = VecDeque::new();
     // let mut q = VecDeque::from(vec![1, 2, 3]);
+
+    q.push_back(1);
+    q.push_back(2);
+    q.push_back(3);
+    println!("{:?}", q);
 }
