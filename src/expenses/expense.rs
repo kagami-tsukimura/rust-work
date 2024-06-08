da3e9038f08e23d4f754d9b713ca015030d44bab
Kagami <shimura.kyohei7617@gmail.com>
2024-06-08 22:21:00 +0900

    auto: expense

--- src/expenses/expense.rs
+++ src/expenses/expense.rs
@@ -1,7 +1,13 @@
+use std::io;
+
 pub fn main() {
     expense();
 }
 
 fn expense() {
-    println!("Hello, expense!");
+    let mut service_type = String::new();
+    println!("実行したい内容を入力してください（0: 登録, 1: 集計）");
+    io::stdin().read_line(&mut service_type).unwrap();
+    // String → u8にparse
+    let service_type: u8 = service_type.trim().parse().expect("数値を入力してください");
 }
