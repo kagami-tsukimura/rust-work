8900887bd657e6d663f5bf34ed7a08d11fef462f
Kagami <shimura.kyohei7617@gmail.com>
2024-06-08 17:58:56 +0900

    auto: output files

--- src/modules/outfiles.rs
+++ src/modules/outfiles.rs
@@ -5,8 +5,5 @@ pub fn main() {
 }
 
 fn outfiles() {
-    let file_path = "./sample1.txt";
-    let mut f = File::create(file_path).expect("Failed to create file");
-    f.write_all(b"Hello, Rust!\n")
-        .expect("Failed to write to file");
+    println!("-----");
 }
