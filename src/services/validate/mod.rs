31458011ef59c4c8445c0690370862b0f7a36457
Kagami <shimura.kyohei7617@gmail.com>
2024-06-09 20:12:37 +0900

    auto: expenses

--- src/services/validate/mod.rs
+++ src/services/validate/mod.rs
@@ -11,14 +11,16 @@ impl InputValidator {
     pub fn validate_register_type(register_type: u8) {
         match register_type {
             0 | 1 => {}
-            _ => panic!("入力値が不正です。"),
+            _ => panic!("登録種別の入力値が不正です。"),
         }
     }
 
     pub fn validate_category_type(register_type: u8, category_type: u8) {
-        match category_type {
-            0 | 1 => {}
-            _ => panic!("入力値が不正です。"),
+        if register_type == 0 {
+            match category_type {
+                0 | 1 | 2 => {}
+                _ => panic!("カテゴリの入力値が不正です。"),
+            }
         }
     }
 }
