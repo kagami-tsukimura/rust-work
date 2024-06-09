0d2c882915cfd73206671fcd0c82e67a38453b1b
Kagami <shimura.kyohei7617@gmail.com>
2024-06-09 18:41:21 +0900

    auto: expenses

--- src/services/io/mod.rs
+++ src/services/io/mod.rs
@@ -4,5 +4,16 @@ use std::io::prelude::*;
 use std::io::BufReader;
 
 pub fn read_data_or_create_new_data(file_path: &str) -> Vec<models::Item> {
-    println!("read_data_or_create_new_data");
+    let file = File::open(file_path);
+
+    match file {
+        Ok(f) => {
+            let buf_reader = BufReader::new(f);
+            serde_json::from_reader(buf_reader).expect("デシリアライズに失敗しました")
+        }
+        Err(_) => {
+            println!("新規ファイルを作成します");
+            Vec::new()
+        }
+    }
 }
